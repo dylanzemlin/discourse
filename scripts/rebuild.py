364d1ff6390dc7b433fc0d18855ea91a89e725c7
Dylan Zemlin <dylansmrw@gmail.com>
2022-11-28 23:32:47 -0600

    Add rebuild.py as a test

---
+++ scripts/rebuild.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+client_name = "discourse_client"
+server_name = "discourse_server"
+
+
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    # Pull latest updates
+    os.system("git pull")
+
+    # Rebuild the projects and wait for it to finish
+    os.system("cd ../client && yarn build")
+
+    # Tell PM2 to restart the processes
+    os.system("pm2 restart " + client_name)
+    os.system("pm2 restart " + server_name)
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=3002)
