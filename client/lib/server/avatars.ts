9e664a53a137737221f81704e3f0d80c85d8dd21
Dylan Zemlin <dylansmrw@gmail.com>
2022-11-30 07:19:39 -0600

    Fix avatar uploading error

--- client/lib/server/avatars.ts
+++ client/lib/server/avatars.ts
@@ -2,14 +2,14 @@ import fs from "fs";
 
 export function getSavedAvatar(uid: string): Buffer {
   if(!fs.existsSync(`./public/avatars/${uid}.png`)) {
-    return fs.readFileSync(`./public/default_avatar.png`);
+    return fs.readFileSync(`./public/default_avatar.jpg`);
   }
 
   return fs.readFileSync(`./public/avatars/${uid}.png`);
 }
 
 export function saveAvatar(uid: string, buffer: Buffer): void {
-  if(fs.existsSync("./public/avatars")) {
+  if(!fs.existsSync("./public/avatars")) {
     fs.mkdirSync("./public/avatars");
   }
   fs.writeFileSync(`./public/avatars/${uid}.png`, buffer);
